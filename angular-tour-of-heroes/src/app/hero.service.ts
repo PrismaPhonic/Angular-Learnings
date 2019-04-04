4acde0d65185f3cf10314924881b75455d703b3b
Peter Farr <Peter@PrismaPhonic.com>
2019-04-03 17:03:16 -0700

    Mocked a database and connected our components to that mock database with http calls that get intercepted

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,23 +2,39 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient,
+  ) { }
 
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _afer_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  private heroesUrl = "api/heroes";
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+
+    let hero: Observable<Hero>;
+    this.http.get<Hero[]>(this.heroesUrl)
+      .subscribe(heroes => {
+        hero = of(heroes.find(hero => hero.id === id));
+      });
+
+    return hero;
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _afer_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 }
