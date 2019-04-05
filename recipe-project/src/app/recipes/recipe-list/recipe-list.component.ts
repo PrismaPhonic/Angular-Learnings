57aaef12b6ce3eb9ab0dd4bf9f5da98d41076050
Peter Farr <Peter@PrismaPhonic.com>
2019-04-05 15:13:31 -0700

    Finished exercise to dynamically load in recipe data, and used structural directive ngFor to loop through our list of recipes and present them visually

--- recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://blog.bulletproof.com/wp-content/uploads/2019/01/54-of-the-Best-Whole30-Recipes-on-the-Internet-_header-752x401.jpg'
+    ),
+  ];
 
   constructor() { }
 
