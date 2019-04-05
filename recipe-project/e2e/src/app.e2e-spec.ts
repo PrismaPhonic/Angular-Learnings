68b427b394a04b769e65ffa018932da336537aa5
Peter Farr <Peter@PrismaPhonic.com>
2019-04-05 14:16:38 -0700

    Got basic component structure setup

--- recipe-project/e2e/src/app.e2e-spec.ts
+++ recipe-project/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to recipe-project!');
+    expect(page.getTitleText()).toEqual('Welcome to recipes-project!');
   });
 
   afterEach(async () => {
--- recipe-project/src/app/app.component.spec.ts
+++ recipe-project/src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'recipe-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('recipe-project');
+    expect(app.title).toEqual('recipes-project');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to recipe-project!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to recipes-project!');
   });
 });
