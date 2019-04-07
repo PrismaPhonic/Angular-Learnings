7b99bde3b50f6f3e18c161dfe76b204dc3d43a32
Peter Farr <Peter@PrismaPhonic.com>
2019-04-06 21:08:02 -0700

    Learned how to use HostListener decorator inside of a custom directive to create methods that react dynamically to changes in the dom

---
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,33 @@
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Renderer3} from '@angular/core/src/render3/interfaces/renderer';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'blue'
+    );
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'transparent'
+    );
+  }
+}
