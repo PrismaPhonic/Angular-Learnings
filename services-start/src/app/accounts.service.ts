e3660b294a7bede071283c65d8345c996af634e7
Peter Farr <Peter@PrismaPhonic.com>
2019-04-07 18:58:30 -0700

    Created an accounts service which holds mock accounts data, and provides addAccounts and updateStatus methods to be called from anywhere.  Refactored components to use this service rather than using event emitters

---
+++ services-start/src/app/accounts.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Account } from './account.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  accounts: Account[] = [
+    new Account('Master Account', 'active'),
+    new Account('Testaccount', 'inactive'),
+    new Account('Hidden Account', 'unknown'),
+  ];
+
+  constructor() { }
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({
+      name,
+      status,
+    });
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+}
