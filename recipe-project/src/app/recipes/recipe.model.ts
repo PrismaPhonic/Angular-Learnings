8fabfdffc57fca0be4a3d74283001dc71e538054
Peter Farr <Peter@PrismaPhonic.com>
2019-04-08 14:38:41 -0700

    Modified recipes to include an ingredients array, and added some mock data so our recipes detail section shows the ingredients for each recipe item

--- recipe-project/src/app/recipes/recipe.model.ts
+++ recipe-project/src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(options: {name: string, desc: string, imagePath: string}) {
+  constructor(options: {name: string, desc: string, imagePath: string, ingredients: Ingredient[]}) {
     this.name = options.name;
     this.description = options.desc;
     this.imagePath = options.imagePath;
+    this.ingredients = options.ingredients;
   }
 }
--- recipe-project/src/app/recipes/recipe.service.ts
+++ recipe-project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import {Recipe} from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,22 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe ({
-      name: 'A Test Recipe',
-      desc: 'This is simply a test',
-      imagePath: 'https://blog.bulletproof.com/wp-content/uploads/2019/01/54-of-the-Best-Whole30-Recipes-on-the-Internet-_header-752x401.jpg'
+      name: 'Schnitzel',
+      desc: 'A tasty schnitzel',
+      imagePath: 'https://thecozyapron.com/wp-content/uploads/2012/02/schnitzel_thecozyapron_1.jpg',
+      ingredients: [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
     }),
     new Recipe ({
-      name: 'Another Test Recipe',
-      desc: 'This is simply a test',
-      imagePath: 'https://blog.bulletproof.com/wp-content/uploads/2019/01/54-of-the-Best-Whole30-Recipes-on-the-Internet-_header-752x401.jpg'
+      name: 'Worcestershire Burger',
+      desc: 'Amazing burger with worcestershire sauce',
+      imagePath: 'https://www.readersdigest.ca/wp-content/uploads/sites/14/2015/11/gourmet-burger-1024x666.jpg',
+      ingredients: [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
     })
   ];
 
