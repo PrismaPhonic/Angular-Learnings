01501b0ee215b65db51fc234e0ec8064f5d03687
Peter Farr <Peter@PrismaPhonic.com>
2019-04-08 17:17:28 -0700

    Learned about url params by making a parametized route, and passed those params to the user component for testing and learning purposes

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const { id, name } = this.route.snapshot.params;
+    this.user = { id, name };
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
-
 }
