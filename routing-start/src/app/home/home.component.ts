6db38b25989cab8078a5d473a396569dc986eff2
Peter Farr <Peter@PrismaPhonic.com>
2019-04-08 17:44:27 -0700

    Learned how to add query params both in routerLinks and programmatically:

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -13,8 +13,15 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     // complex calculation - reach out to backend etc.
-    this.router.navigate(['/servers']);
+    this.router.navigate(
+      ['/servers', id, 'edit'],
+      {
+        queryParams: {
+          allowEdit: '1'
+        },
+        fragment: 'loading'
+      });
   }
 }
