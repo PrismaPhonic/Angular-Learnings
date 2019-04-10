b792c513dce9eb99015b88d5f0b567b1f9da28d1
Peter Farr <Peter@PrismaPhonic.com>
2019-04-10 14:36:25 -0700

    Refactored shopping list service to use a Subject rather than an EventEmitter, because EventEmitter is only intended for communication between child and parent, and should not be subscribed to

--- Udemy-Angular-Complete-Guide-Course-Material/recipe-project/src/app/shopping-list/shopping-list.service.ts
+++ Udemy-Angular-Complete-Guide-Course-Material/recipe-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-  ingredientsUpdated = new EventEmitter<Ingredient[]>();
+  ingredientsUpdated = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -20,11 +21,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsUpdated.emit(this.getShoppingList());
+    this.ingredientsUpdated.next(this.getShoppingList());
   }
 
   addIngredients(newIngredients: Ingredient[]) {
     this.ingredients = [...this.ingredients, ...newIngredients];
-    this.ingredientsUpdated.emit(this.getShoppingList());
+    this.ingredientsUpdated.next(this.getShoppingList());
   }
 }
