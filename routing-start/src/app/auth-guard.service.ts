0c03bc8ba2c1115e368302d8336112d8056487b4
Peter Farr <Peter@PrismaPhonic.com>
2019-04-09 20:47:19 -0700

    Created login and logout buttons that call our fake login and logout auth service methods.  Tested to show that the basic functionality is working, and the guards are blocking behavior appropriately

--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -1,12 +1,13 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import { st } from '@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService,
               private router: Router) {}
 
@@ -21,5 +22,9 @@ export class AuthGuard implements CanActivate {
           }
         }
       );
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(childRoute, state);
   }
 }
