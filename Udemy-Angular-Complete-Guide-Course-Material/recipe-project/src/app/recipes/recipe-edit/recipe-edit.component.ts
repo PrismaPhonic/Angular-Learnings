dc352919557a8d9c79fde5991466d2edf067ba0c
Peter Farr <Peter@PrismaPhonic.com>
2019-04-12 13:52:17 -0700

    Added an Add Ingredient button to the recipe edit form, which upon clicking dynamically adds more input fields

--- Udemy-Angular-Complete-Guide-Course-Material/recipe-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Udemy-Angular-Complete-Guide-Course-Material/recipe-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -35,7 +35,7 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
-    let recipeIngredients = new FormArray([]);
+    const recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
@@ -64,5 +64,14 @@ export class RecipeEditComponent implements OnInit {
 
   onSubmit() {
     console.log(this.recipeForm);
+  }
+
+  addIngredient() {
+    this.ingredients.push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl(),
+      })
+    );
   }
 }
